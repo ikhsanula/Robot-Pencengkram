71bfb8f3c4193bc8e16601488a64394ff3e752d4
ikhsanula <88315426+ikhsanula@users.noreply.github.com>
2021-08-04 21:45:12 +0700

    Create pwm_control.py

---
+++ pwm/pwm_control.py
@@ -0,0 +1,10 @@
+import os, time
+
+os.system("sudo pkill -f generate_pwm.py")
+print("stopped !!!")
+
+#time.sleep(0.1)
+
+print("starting pwm")
+os.system("python /var/www/html/robot/pwm/generate_pwm.py &")
+print("started !!!")
